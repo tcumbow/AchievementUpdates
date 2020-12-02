22b5fd35afcaa9c673e10f28765f444e1ff4c214
Tom Cumbow <tcumbow@icloud.com>
2020-12-02 16:56:35 -0500

    folderized

---
+++ Sync.ps1
@@ -0,0 +1,5 @@
+unison .\AchievementUpdates A:\AchievementUpdates -auto -batch
+pause
+unison .\AchievementUpdates R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AchievementUpdates -auto -batch
+pause
+unison .\AchievementUpdates "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AchievementUpdates" -auto -batch
