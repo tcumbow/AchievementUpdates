5c23a850d5373df8aeb737b7482b5e2492a8d45f
Tom Cumbow <tcumbow@icloud.com>
2020-12-29 18:07:56 -0500

    Update Sync.ps1

--- Sync.ps1
+++ Sync.ps1
@@ -1,5 +1,4 @@
 unison .\AchievementUpdates A:\AchievementUpdates -auto -batch
 pause
-unison .\AchievementUpdates R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AchievementUpdates -auto -batch
-pause
-unison .\AchievementUpdates "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AchievementUpdates" -auto -batch
+robocopy .\AchievementUpdates R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AchievementUpdates /mir
+robocopy .\AchievementUpdates "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AchievementUpdates" /mir
